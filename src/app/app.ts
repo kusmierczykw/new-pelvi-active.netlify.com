0caea0e4d7f0e64d3a6c25b1b823aa2a817f044a
Wojciech Kuśmierczyk <wojciech.kusmierczyk@outlook.com>
2026-01-11 20:24:19 +0100

    Praca nad sekcją "O mnie"

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeroSection } from './sections/hero-section/hero-section';
 import { Navbar } from '@ui/navbar/navbar';
 import { Booksy } from '@ui/booksy/booksy';
+import { AboutMeSection } from './sections/about-me-section/about-me-section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroSection, Navbar, Booksy],
+  imports: [RouterOutlet, HeroSection, Navbar, Booksy, AboutMeSection],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/sections/about-me-section/about-me-section.ts
+++ src/app/sections/about-me-section/about-me-section.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Fullscreen } from '@ui/fullscreen/fullscreen';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-about-me-section',
-  imports: [Fullscreen],
+  imports: [Fullscreen, NgOptimizedImage],
   templateUrl: './about-me-section.html',
   styleUrl: './about-me-section.scss',
 })
