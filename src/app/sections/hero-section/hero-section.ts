284f2711af8bc1c3737e87aaf88dc9df9a385bc4
Wojciech Kuśmierczyk <wojciech.kusmierczyk@outlook.com>
2026-02-02 16:55:56 +0100

    Praca nad naprawieniem Video w sekcji hero.

--- src/app/sections/hero-section/hero-section.ts
+++ src/app/sections/hero-section/hero-section.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  inject,
+  PLATFORM_ID,
+  viewChild,
+} from '@angular/core';
 import { Scrolling } from '@ui/components/scrolling/scrolling';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-hero-section',
@@ -7,4 +15,22 @@ import { Scrolling } from '@ui/components/scrolling/scrolling';
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.scss',
 })
-export class HeroSection {}
+export class HeroSection implements AfterViewInit {
+  private readonly videoElementRef =
+    viewChild.required<ElementRef<HTMLVideoElement>>('videoElementRef');
+  private readonly platform = inject(PLATFORM_ID);
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platform)) {
+      const videoElementRef = this.videoElementRef().nativeElement;
+
+      if (!videoElementRef) {
+        return;
+      }
+
+      videoElementRef.playbackRate = 0.8;
+
+      void videoElementRef.play();
+    }
+  }
+}
